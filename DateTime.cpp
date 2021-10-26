0099b77d00c4136eb2639850c0e2b6285f2b53b8
Mark Nikulin <moirted@gmail.com>
2021-10-26 12:42:48 +0300

    DaysInYear and DaysInYearRange realization

--- DateTime.cpp
+++ DateTime.cpp
@@ -1,7 +1,9 @@
 #include "DateTime.h"
+#include <assert.h>
 
 bool IsLeapYear(int y)
 {
+	assert(y > 0);
 	if (((y % 100 != 0) && (y % 4 == 0)) || ((y % 100 == 0) && (y % 400 == 0)))
 	{
 		return true;
@@ -10,4 +12,36 @@ bool IsLeapYear(int y)
 	{
 		return false;
 	}
+}
+
+bool LaterInYear(int a, int b)
+{
+	assert(a < 32);
+	assert(a > 0);
+	assert(b < 32);
+	assert(b > 0);
+	return a > b;
+}
+
+int DaysInYear(int y)
+{
+	if (IsLeapYear(y))
+	{
+		return 366;
+	}
+	else
+	{
+		return 365;
+	}
+}
+
+int DaysInYearRange(int y1, int y2)
+{
+	assert(y1 <= y2);
+	int res;
+	for (int i = y1; i <= y2; i++)
+	{
+		res += DaysInYear(i);
+	}
+	return res;
 }
